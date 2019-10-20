3bf6dca9b670242e181e50280b6f7c91c9ec687f
Denis Onder <denis_onder@protonmail.com>
2019-10-20 14:45:10 +0200

    New implementation, fixed the commit message

--- script.sh
+++ script.sh
@@ -1,40 +1,11 @@
 #!/usr/bin/env bash
 
-function get_branch_name {
-    # main string
-    str=`git status | grep branch`
-    # delimiter string
-    delimiter=" "
-    #length of main string
-    strLen=${#str}
-    #length of delimiter string
-    dLen=${#delimiter}
-    #iterator for length of string
-    i=0
-    #length tracker for ongoing substring
-    wordLen=0
-    #starting position for ongoing substring
-    strP=0
-    
-    array=()
-    while [ $i -lt $strLen ]; do
-        if [ $delimiter == ${str:$i:$dLen} ]; then
-            array+=(${str:strP:$wordLen})
-            strP=$(( i + dLen ))
-            wordLen=0
-            i=$(( i + dLen ))
-        fi
-        i=$(( i + 1 ))
-        wordLen=$(( wordLen + 1 ))
-    done
-    array+=(${str:strP:$wordLen})
-    
-    GIT_BRANCH_NAME=${array[-1]}
-}
-
+# Get path to the directory from which the script is being called
 SCRIPT_PATH=`pwd`
 cd $SCRIPT_PATH
-get_branch_name
+# Get current git branch
+GIT_BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+# ???
 read -p "Commit message: " GIT_COMMIT_MSG
-
+# Profit
 git add . && git commit -m "$GIT_COMMIT_MSG" && git push origin $GIT_BRANCH_NAME

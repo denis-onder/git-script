3d0a82a325387c6331e2b8f0be9f61a3170e9f15
Denis Onder <denis_onder@protonmail.com>
2020-04-21 02:30:01 +0200

    Updated installer script and made sure that the upstream is set to the current branch on push

--- install.sh
+++ install.sh
@@ -1,3 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-sudo cp ./script.sh /usr/local/bin/ && cd /usr/local/bin/ && sudo mv script.sh gitScript
+FILE=/usr/local/bin/gitcp
+
+if [ -f "$FILE" ]; then
+	sudo rm /usr/local/bin/gitcp
+fi
+
+sudo cp ./script.sh /usr/local/bin/ && cd /usr/local/bin/ && sudo mv script.sh gitcp
